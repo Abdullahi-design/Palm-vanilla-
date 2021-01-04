995af6ed9753aa9f9661bd96a54a5b041d6f2589
M.Abdul <66181258+Abdullahi-design@users.noreply.github.com>
2021-01-04 10:58:16 +0100

    Palm

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      color: {        
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
